115e948cfd508039ecca80bb9fa5812491dd1e2e
sibStephen <sibu.stephen@srijan.in>
2016-07-01 00:55:42 +0530

    Backup

---
+++ Shishir/Gruntfile.js
@@ -0,0 +1,41 @@
+/*jslint node: true */
+"use strict";
+
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    //connect
+    connect: {
+      server: {
+        options: {
+          hostname: '127.0.0.1',
+          port: 8282
+        }
+      }
+    },
+    //grunt server
+    'http-server': {
+      'dev': {
+        port: 8282,
+        openBrowser: true
+      }
+    },
+    wiredep: {
+      task: {
+        src: ['index.html']
+      }
+    },
+    files: {
+      'index.html': [
+        'dashboard/*.js',
+        'index.js',
+        'css/*.css',
+      ],
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-http-server');
+};
